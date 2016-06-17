f740a9cfae950c2928e076477f955a400247373d
paride <paride.casulli@gmail.com>
2016-06-17 12:04:04 +0200

    storm path

--- Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/singletons/Settings.java
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/singletons/Settings.java
@@ -3,5 +3,5 @@ package singletons;
 public class Settings {
 	public static int 		decisionInterval	=	120000;	//storm publishes datas every 60 sec
 	public static String	topologyName		=	"topolox";
-	public static String	stormPath			=	"/home/paride/Scrivania/apache-storm-1.0.1/bin/";
+	public static String	stormPath			=	"/home/paride/apache-storm-1.0.1/bin/";
 }
