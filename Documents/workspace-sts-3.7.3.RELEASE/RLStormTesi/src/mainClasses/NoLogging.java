cc7b1b6828f3ba23f68ba0bb84b3ee21cf75306a
paride <paride.casulli@gmail.com>
2016-06-17 12:35:47 +0200

    fix

---
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/mainClasses/NoLogging.java
@@ -0,0 +1,91 @@
+package mainClasses;
+
+import org.eclipse.jetty.util.log.Logger;
+
+public class NoLogging implements Logger {
+
+	@Override
+	public void debug(Throwable arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void debug(String arg0, Object... arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void debug(String arg0, Throwable arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Logger getLogger(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void ignore(Throwable arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void info(Throwable arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void info(String arg0, Object... arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void info(String arg0, Throwable arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isDebugEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setDebugEnabled(boolean arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void warn(Throwable arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void warn(String arg0, Object... arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void warn(String arg0, Throwable arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
