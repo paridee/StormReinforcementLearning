4f70ccda15d1f30db7b1b41e1273fb42da153cae
paride <paride.casulli@gmail.com>
2016-07-01 00:20:47 +0200

    fix segno

--- Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/rl/rewarder/DeltaRewarder.java
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/rl/rewarder/DeltaRewarder.java
@@ -1,6 +1,10 @@
 package rl.rewarder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DeltaRewarder implements RewardCalculator {
+	private static final Logger LOG = LoggerFactory.getLogger(DeltaRewarder.class);
 	private double oldDistance;
 	private int lowerBound;
 	private int obj;
@@ -40,8 +44,9 @@ public class DeltaRewarder implements RewardCalculator {
 		}
 		double delta		=	oldDistance-newDistance;
 		delta				=	delta/1000;
+		LOG.debug("delta distance "+(delta*1000)+" ms, instance delta penalty contribution "+(this.costPerInstance*(this.oldExNumber-singletons.SystemStatus.getOperatorsLevel()))+" delta contribution "+(delta/1000));
 		//if more executor than past has to be a cost
-		delta	=	delta-(this.costPerInstance*(this.oldExNumber-singletons.SystemStatus.getOperatorsLevel()));
+		delta	=	delta+(this.costPerInstance*(this.oldExNumber-singletons.SystemStatus.getOperatorsLevel()));
 		this.oldExNumber	=	singletons.SystemStatus.getOperatorsLevel();
 		this.oldDistance	=	newDistance;
 		return delta;
