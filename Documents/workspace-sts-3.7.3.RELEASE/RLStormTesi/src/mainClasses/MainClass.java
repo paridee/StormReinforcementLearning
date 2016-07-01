7cdeaa95675e7867f44e6cc637bac4bfc9de6e9a
paride <paride.casulli@gmail.com>
2016-07-01 23:55:34 +0200

    fix

--- Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/mainClasses/MainClass.java
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/mainClasses/MainClass.java
@@ -60,7 +60,7 @@ public class MainClass {
 		
 		//RewardCalculator					 	rewarder	=	new ParabolicComplexResponseTimeRewarder(3000,125,4500,ACTIONS_NUM);
 		//RewardCalculator					 	rewarder	=	new DeltaRewarder(3000,4500,15,0.2);
-		RewardCalculator					 	rewarder	=	new DeltaRewarderSimplified(300,3000);
+		RewardCalculator					 	rewarder	=	new DeltaRewarderSimplified(300,3000,4500);
 		ProcessTimeStateReader					reader		=	new ProcessTimeStateReader(3000,0.5,1.5);
 		FixedIntervalManager					intManager	=	new FixedIntervalManager(Settings.decisionInterval);
 		//WorkerNumberExecutor					executor	=	new WorkerNumberExecutor(rewarder,intManager);
