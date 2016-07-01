a22e3c4b5c16bccbeef9b060cfd7cf146d383a87
paride <paride.casulli@gmail.com>
2016-07-01 23:52:22 +0200

    improvedRewarder

--- Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/rl/rewarder/DeltaRewarderSimplified.java
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/rl/rewarder/DeltaRewarderSimplified.java
@@ -3,15 +3,19 @@ package rl.rewarder;
 public class DeltaRewarderSimplified implements RewardCalculator {
 	int distThreshold;
 	int oldInstanceNumber;
+	int upperBound;
+	int lowerBound;
 	double oldDistance;
 	int obj;
 	
-	public DeltaRewarderSimplified(int distThreshold, int obj) {
+	public DeltaRewarderSimplified(int distThreshold, int obj,int upperBound) {
 		super();
 		this.distThreshold = distThreshold;
 		this.obj = obj;
 		oldInstanceNumber	=	singletons.SystemStatus.getOperatorsLevel();
 		this.oldDistance	=	singletons.SystemStatus.processLatency-obj;	
+		this.upperBound		=	upperBound;
+		lowerBound			=	obj-(upperBound-obj);
 	}
 
 
@@ -34,6 +38,10 @@ public class DeltaRewarderSimplified implements RewardCalculator {
 		}
 		else if(machineDelta<0){
 			reward	=	reward-0.5;
+		}
+		double currentLatency	=	singletons.SystemStatus.processLatency;
+		if((currentLatency<this.lowerBound)||(currentLatency>this.upperBound)){
+			reward	=	reward - 0.5;
 		}
 		return reward;
 	}
