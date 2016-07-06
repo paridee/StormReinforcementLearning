d56475166ebcde43172077d111a2fd95de42fd86
paride <paride.casulli@gmail.com>
2016-07-06 20:58:13 +0200

    restored rewarder

--- Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/mainClasses/MainClass.java
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/mainClasses/MainClass.java
@@ -78,8 +78,8 @@ public class MainClass {
 		
 		//RewardCalculator					 	rewarder	=	new ParabolicComplexResponseTimeRewarder(3000,125,4500,ACTIONS_NUM);
 		//RewardCalculator					 	rewarder	=	new DeltaRewarder(3000,4500,15,0.2);
-		//RewardCalculator					 	rewarder	=	new DeltaRewarderSimplified(300,3000,4500);
-		RewardCalculator					 	rewarder	=	new CongestionDeltaRewarder(boltsName,4500,3000);
+		RewardCalculator					 	rewarder	=	new DeltaRewarderSimplified(300,3000,4500);
+		//RewardCalculator					 	rewarder	=	new CongestionDeltaRewarder(boltsName,4500,3000);
 		ProcessTimeStateReader					reader		=	new ProcessTimeStateReader(3000,0.5,1.5);
 		FixedIntervalManager					intManager	=	new FixedIntervalManager(Settings.decisionInterval);
 		//WorkerNumberExecutor					executor	=	new WorkerNumberExecutor(rewarder,intManager);
