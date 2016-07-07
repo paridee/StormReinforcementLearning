d262c0da638a9fd16fccb0fbd31daf8840442079
paride <paride.casulli@gmail.com>
2016-07-07 17:32:25 +0200

    fix

--- Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/linearGradientSarsa/LinearGradientDescendSarsaLambda.java
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/linearGradientSarsa/LinearGradientDescendSarsaLambda.java
@@ -51,116 +51,116 @@ public class LinearGradientDescendSarsaLambda implements Runnable {
 		action			=	this.initAction;
 	}*/
 
-	void partTwo(){
 
-	}
 	@Override
 	public void run() {
-		int[] features	=	null;
-		currentState	=	reader.getCurrentState();	//read state
-		System.out.println("features for state "+currentState+" "+action);
-		try {
-			features		=	eval.getFeatures(currentState, action);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		System.out.println("feat length "+features.length+" "+features[features.length-1]);
-		for(int i=0;i<features.length;i++){
-			if(features[i]==1){
-				eVector[i]	=	1;
+		while(true){
+			int[] features	=	null;
+			currentState	=	reader.getCurrentState();	//read state
+			System.out.println("features for state "+currentState+" "+action);
+			try {
+				features		=	eval.getFeatures(currentState, action);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-			System.out.print(features[i]+"\t");
-		}
-		System.out.println("\n");
-
-		double reward	=	-Double.MAX_VALUE;
-		try {
-			reward	=	executor.execute(action,currentState);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
-		currentState	=	reader.getCurrentState();
-		double delta	=	reward;
-		for(int i=0;i<features.length;i++){
-			if(features[i]==1){
-				delta		=	delta	+	omega[i];	
+			System.out.println("feat length "+features.length+" "+features[features.length-1]);
+			for(int i=0;i<features.length;i++){
+				if(features[i]==1){
+					eVector[i]	=	1;
+				}
+				System.out.print(features[i]+"\t");
 			}
-		}
-		double randomV			=	Math.random();
-		double qActionChoosen	=	0;
-		if(randomV>epsilon){
-			//exploitation
-			double Q[]	=	new double[actions];
-			for(int i=0;i<actions;i++){
+			System.out.println("\n");
+	
+			double reward	=	-Double.MAX_VALUE;
+			try {
+				reward	=	executor.execute(action,currentState);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
+			currentState	=	reader.getCurrentState();
+			double delta	=	reward;
+			for(int i=0;i<features.length;i++){
+				if(features[i]==1){
+					delta		=	delta	+	omega[i];	
+				}
+			}
+			double randomV			=	Math.random();
+			double qActionChoosen	=	0;
+			if(randomV>epsilon){
+				//exploitation
+				double Q[]	=	new double[actions];
+				for(int i=0;i<actions;i++){
+					try {
+						features	=	eval.getFeatures(currentState, i);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					for(int j=0;j<features.length;j++){
+						Q[i]	=	Q[i]	+	(omega[j]*features[j]);
+					}
+				}
+				
+				//testing
+				for(int i=0;i<actions;i++){
+					System.out.println("Q["+currentState+"]["+i+"] "+Q[i]+"\t");
+				}
+				
+				
+				//find best action
+				int newAction	=	0;
+				
+				System.out.println("number of actions "+actions);
+				
+				double qAction	=	Q[0];
+				for(int j=1;j<actions;j++){
+					if(Q[j]>qAction){
+						newAction	=	j;
+						qAction		=	Q[j];
+					}
+				}
+				action			=	newAction;
+				qActionChoosen	=	qAction;
+				System.out.println("Exploiation:Current state: "+currentState+" action "+action);
+				//best action found
+			}
+			else{
+				//exploration
+				action			=	(int)((Math.random()*actions)%actions);
 				try {
-					features	=	eval.getFeatures(currentState, i);
+					features		=	eval.getFeatures(currentState, action);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				double tempQ	=	0;
 				for(int j=0;j<features.length;j++){
-					Q[i]	=	Q[i]	+	(omega[j]*features[j]);
+					tempQ	=	tempQ	+	(omega[j]*features[j]);
 				}
+				qActionChoosen	=	tempQ;
+				
+				//testing
+				System.out.println("Exploration:Current state: "+currentState+" action "+action);
+				System.out.println("Q["+currentState+"]["+action+"] "+qActionChoosen+"\t");
+				
+			}
+			delta	=	delta	+	(yota*qActionChoosen);
+			for(int i=0;i<featuresN;i++){
+				omega[i]	=	omega[i]+(alphaCalculator.getAlpha(action)*delta*eVector[i]);
+				eVector[i]	=	yota*lambda*eVector[i];
 			}
 			
-			//testing
-			for(int i=0;i<actions;i++){
-				System.out.println("Q["+currentState+"]["+i+"] "+Q[i]+"\t");
+			System.out.println("Omega vector:");
+			for(int i=0;i<featuresN;i++){
+				System.out.print(omega[i]+"\t");
 			}
-			
-			
-			//find best action
-			int newAction	=	0;
-			
-			System.out.println("number of actions "+actions);
-			
-			double qAction	=	Q[0];
-			for(int j=1;j<actions;j++){
-				if(Q[j]>qAction){
-					newAction	=	j;
-					qAction		=	Q[j];
-				}
-			}
-			action			=	newAction;
-			qActionChoosen	=	qAction;
-			System.out.println("Exploiation:Current state: "+currentState+" action "+action);
-			//best action found
+			System.out.println("\nTrace vector:");
+			for(int i=0;i<featuresN;i++){
+				System.out.print(eVector[i]+"\t");
+			}		
 		}
-		else{
-			//exploration
-			action			=	(int)((Math.random()*actions)%actions);
-			try {
-				features		=	eval.getFeatures(currentState, action);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			double tempQ	=	0;
-			for(int j=0;j<features.length;j++){
-				tempQ	=	tempQ	+	(omega[j]*features[j]);
-			}
-			qActionChoosen	=	tempQ;
-			
-			//testing
-			System.out.println("Exploration:Current state: "+currentState+" action "+action);
-			System.out.println("Q["+currentState+"]["+action+"] "+qActionChoosen+"\t");
-			
-		}
-		delta	=	delta	+	(yota*qActionChoosen);
-		for(int i=0;i<featuresN;i++){
-			omega[i]	=	omega[i]+(alphaCalculator.getAlpha(action)*delta*eVector[i]);
-			eVector[i]	=	yota*lambda*eVector[i];
-		}
-		
-		System.out.println("Omega vector:");
-		for(int i=0;i<featuresN;i++){
-			System.out.print(omega[i]+"\t");
-		}
-		System.out.println("\nTrace vector:");
-		for(int i=0;i<featuresN;i++){
-			System.out.print(eVector[i]+"\t");
-		}		
 	}
 }
