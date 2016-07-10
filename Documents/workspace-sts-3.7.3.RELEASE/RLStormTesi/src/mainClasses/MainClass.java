de077e8f5bc47667674e55e881b55524bc6090c0
paride <paride.casulli@gmail.com>
2016-07-10 18:27:24 +0200

    changed policy

--- Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/mainClasses/MainClass.java
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/mainClasses/MainClass.java
@@ -88,7 +88,7 @@ public class MainClass {
 		ProcessTimeStateReader					reader		=	new ProcessTimeStateReader(3000,0.5,1.5);
 		FixedIntervalManager					intManager	=	new FixedIntervalManager(Settings.decisionInterval);
 		//WorkerNumberExecutor					executor	=	new WorkerNumberExecutor(rewarder,intManager);
-		rl.policies.PolicyChooser				chooser		;//=	new rl.policies.SoftmaxPolicyChooser(0.2);//EpsilonGreedyChooser(0.1);
+		rl.policies.PolicyChooser				chooser		=	new rl.policies.SoftmaxPolicyChooser(0.2);//EpsilonGreedyChooser(0.1);
 		StaticAlphaCalculator					alpha		=	new StaticAlphaCalculator(0.8);
 		//Thread sarsaThread									=	new Thread(sarsa);
 		//sarsaThread.start();
@@ -105,7 +105,7 @@ public class MainClass {
 		steps[2]	=	4;
 		ExecutorsChange							executor	=	new ExecutorsChange(boltsName,steps, 32, singletons.Settings.topologyName,intManager,rewarder);
 		//BottleneckExecutor							executor	=	new BottleneckExecutor(rewarder,intManager,32);
-		chooser	=	new EpsilonGreedyWithFeasibilityCheck(executor,0.1);
+		//chooser	=	new EpsilonGreedyWithFeasibilityCheck(executor,0.1);
 		
 		//ExpectedSarsa							sarsa		=	new	ExpectedSarsa(3,actionsN,1,chooser,executor,reader,alpha,"QMatrix.txt");
 		//Thread									sarsaTh		=	new Thread(sarsa);
