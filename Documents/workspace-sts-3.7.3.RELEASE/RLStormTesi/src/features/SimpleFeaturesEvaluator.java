703ed6072981e433fc01eadadb2653c05c9d00a7
paride <paride.casulli@gmail.com>
2016-07-14 19:12:19 +0200

    changed trace mode

--- Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/features/SimpleFeaturesEvaluator.java
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/features/SimpleFeaturesEvaluator.java
@@ -49,7 +49,7 @@ public class SimpleFeaturesEvaluator implements FeaturesEvaluator {
 				else{
 					features[(state*this.featuresPerState)+4]	=	1;
 				}
-				//features[(state*this.featuresPerState)+6]	=	1;
+				features[(state*this.featuresPerState)+6]	=	1;
 			}
 			else{
 				String op	=	opName.get(operator);
@@ -60,7 +60,7 @@ public class SimpleFeaturesEvaluator implements FeaturesEvaluator {
 				else{
 					features[(state*this.featuresPerState)+3]	=	1;
 				}
-				//features[(state*this.featuresPerState)+7]	=	1;
+				features[(state*this.featuresPerState)+7]	=	1;
 			}
 			//System.out.print(" Action "+actionV+" on operator "+operator+"\n");
 		}
