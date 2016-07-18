07de0edf3ebaf941a0e683c734319639b7f99b05
paride <paride.casulli@gmail.com>
2016-07-18 18:41:33 +0200

    restored wait on rebalance

--- Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/rl/executer/ExecutorsChangeMultipleSteps.java
+++ Documents/workspace-sts-3.7.3.RELEASE/RLStormTesi/src/rl/executer/ExecutorsChangeMultipleSteps.java
@@ -186,7 +186,7 @@ public class ExecutorsChangeMultipleSteps implements ActionExecutor {
 		}
 		MainClass.PARALLELISM_VAL.set(totalExecutors);
 		singletons.SystemStatus.workerNumber	=	totalExecutors;
-		String command	=	singletons.Settings.stormPath+"storm rebalance "+this.topologyName+" -w 1 -n "+totalExecutors+execFlags;
+		String command	=	singletons.Settings.stormPath+"storm rebalance "+this.topologyName+" -n "+totalExecutors+execFlags;
 		logger.debug("sending command "+command);
 		Runtime rt 		= 	Runtime.getRuntime();
 		try {
